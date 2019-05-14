8cb651069e35649386b7d9f96a14cde1f6f6ec5d
Bogdan <volodin.bi@yandex.ru>
2019-05-14 13:00:51 +0300

    [Travis-CI] Fix build script

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-cmd.exe /C MSBuild.exe Simple_YouTube_Music_Player.sln /m:2 /nologo /verbosity:minimal /p:Configuration=Release /p:Platform="Any CPU" /t:restore
+cmd.exe /C MSBuild.exe Simple_YouTube_Music_Player.sln /m:2 /nologo /verbosity:minimal /p:Configuration=Release /p:Platform="Any CPU" /t:restore /t:build
+pause
