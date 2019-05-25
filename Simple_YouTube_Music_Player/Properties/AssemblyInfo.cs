86d2efdf22818ab9c3d5c7661dd44ba3bde6e23b
Bogdan <undex.project@gmail.com>
2019-05-25 18:06:34 +0300

    Обновление исправляющее фатальную ошибку

--- Simple_YouTube_Music_Player/Properties/AssemblyInfo.cs
+++ Simple_YouTube_Music_Player/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.8")]
-[assembly: AssemblyFileVersion("2.0.0.8")]
+[assembly: AssemblyVersion("2.0.0.9")]
+[assembly: AssemblyFileVersion("2.0.0.9")]
